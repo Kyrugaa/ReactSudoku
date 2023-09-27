c83bdb909e89fff81a7780c63afd2a5c4ca31210
Kyrugaa <80152027+Kyrugaa@users.noreply.github.com>
2023-09-27 08:15:40 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,150 @@
+import React, { useState } from 'react';
+import './App.css';
+
+
+
+function Sudoku() {
+  // Sudoku-Gitter initialisieren
+  var board = [
+    [' ', '5', '1', '3', '6', '2', '7', ' ', ' '],
+    [' ', '4', ' ', ' ', '5', '8', ' ', ' ', ' '],
+    [' ', ' ', ' ', '4', ' ', ' ', ' ', '2', '5'],
+    [' ', '8', ' ', ' ', ' ', ' ', '9', ' ', '3'],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    ['7', ' ', '5', ' ', ' ', ' ', ' ', '8', ' '],
+    ['1', '2', ' ', ' ', ' ', '9', ' ', ' ', ' '],
+    [' ', ' ', ' ', '2', '8', ' ', ' ', '6', ' '],
+    [' ', ' ', '8', '5', '3', '4', '2', '9', ' ']
+  ]
+
+  // Nimmt board als input und sucht nach erste leere position
+  function nextEmptySpot(board){
+    for(var i = 0; i < 9; i++){
+      for(var j = 0; j< 9; j++){
+        if(board[i][j] === 0)
+        return [i][j]
+      }
+    }
+    return [-1][-1]
+  }
+
+  // Nimmt board Argument für die Reihe die wir checken und einen neuen Wert geben wollen
+  function checkRow(board, row, value){
+    for(var i = 0; i < board[row].length; i++){
+      if(board[row][i] === value){
+        return false
+      }
+    }
+      return true
+  }
+
+  // Nimmt board Argument für die Spalte die wir checken und einen neuen Wert geben wollen
+  function checkColumn(board, col, value){
+    for(var i = 0; i < board[col].length; i++){
+      if(board[col][i] === value){
+        return false
+      }
+    }
+      return true
+  }
+
+
+  // Nimmt board Argument für das Square und schaut ob schon vorhanden
+  function checkSquare(board, row, col, value){
+    //boxRow = Math.floor(row / 3) * 3;
+    //boxCol = Math.floor(col / 3) * 3;
+
+    for(var r = 0; r < 3; r++){
+      for(var c = 0; c < 3; c++){
+      //  if(board[boxRow + r][boxCol + c] === value)
+          return false;
+      }
+    }
+      return true
+  }
+
+
+  // Lösung finden (Backtracking)
+  function solve(board){
+    let emptySpot = nextEmptySpot(board)
+    let row = emptySpot[0]
+    let col = emptySpot[1]
+
+    // Falls keine leeren Stellen mehr
+    if(row === -1){
+      return board
+    }
+
+    for(let num = 1; num <= 9; num++){
+      if(checkValue(board, row, col, num)){
+        board[row][col] = num
+        solve(board)
+      }
+    }
+
+    if(nextEmptySpot(board)[0] !== -1)
+      board[row][col] = 0
+    
+
+    return board
+    
+
+
+  }
+
+  // Alle check-Funktionen zusammenbringen
+  function checkValue(board, row, col, value){
+    if(checkRow(board, row, value) && 
+      checkColumn(board, col, value) && 
+      checkSquare(board, row, col, value)){
+      return true
+    }
+    return false
+  }
+
+
+
+  // zustand um Sudoku-Gitter zu speichern
+  const [sudokuGrid, setSudokuGrid] = useState(board);
+
+
+  // ändern einer Zelle im Sudoku-Gitter
+  function updateCell(row, col, value) {
+    const newGrid = [...sudokuGrid];    //kopieren des aktuellen gitters
+    newGrid[row][col] = value;          //aktualisieren der Zelle mit neuem Wert
+    setSudokuGrid(newGrid);             //aktualisieren des Zustands mit dem neuen Gitter
+  }
+
+
+
+
+
+  return (
+    <div id='div'>
+      <button>Go{solve}</button>
+      {sudokuGrid.map(function(row, rowIndex) {
+        return (
+          <div key={rowIndex}>
+            {row.map(function(cell, colIndex) {
+              return (
+                <input id='input'
+                  key={colIndex}
+                  type="number"
+                  value={cell}
+                  onChange={function(e) {
+                    updateCell(rowIndex, colIndex, parseInt(e.target.value, 10));
+                  }}
+                  min="1"
+                  max="9"
+                />
+                );
+            })}
+             
+          </div>
+        );
+      })}
+    </div>
+  );
+  
+}
+export default Sudoku;
